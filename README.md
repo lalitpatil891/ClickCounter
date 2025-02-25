d1fb08d7874c217013dbad153f95aba913c4df73
Lalit Patil <lp613408@gmail.com>
2025-02-25 19:47:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# ClickCount
+
+A simple and interactive counter app built using React. Tap to increase or decrease the count, and reset it anytime with a single click!
+
+## 🚀 Features
+- **Increment & Decrement**: Click `+` to increase and `-` to decrease the count.
+- **Reset Functionality**: Click the reset button to set the count back to zero.
+- **Minimal & Stylish UI**: Dark-themed design with smooth button interactions.
+
+## 🛠️ Technologies Used
+- **React**: Component-based UI development.
+- **CSS**: Custom styling for an enhanced user experience.
+
+## 📸 Preview
+![ClickCount Preview](/sc/sc1.jpg)
+![ClickCount Preview](/sc/sc2.jpg)
+![ClickCount Preview](/sc/sc3.jpg)
+
+## 📂 Installation & Usage
+```sh
+# Clone the repository
+git clone https://github.com/lalitpatil891/Code-With-React.git
+
+# Navigate to the project directory
+cd ClickCount
+
+# Install dependencies
+npm install
+
+# Start the development server
+npm start
+```
+
+## 🔗 Live Demo
+[ClickCount Live](https://clickcountbylp.netlify.app)
+
+## 🏆 Author
+Developed with ❤️ by [Lalit Patil](https://github.com/lalitpatil891)
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState } from 'react';
+import './App.css'
+
+export default function App() {
+  const [c, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="main-container">
+        <img className='background-glow' src="/grid.png" alt="" />
+        {/* <img className='background-glow' src="/1-shadow.png" alt="" /> */}
+
+        <div className='middle-container'>
+
+          <h2>ClickCount</h2>
+          <p>Tap to add, tap to subtract—it's that simple.</p>
+          <h1>{c}</h1>
+
+          <div className="btn-box">
+            <div className="btn"><button className='button-2' onClick={() => setCount(c - 1)}>-</button></div>
+            <div className="btn">
+              <button className='button-3' onClick={() => setCount(0)}><img className='btn-icon' src="/reset-left-line.png" alt="" /></button>
+            </div>
+            <div className="btn"><button className='button-1' onClick={() => setCount(c + 1)}>+</button></div>
+
+          </div>
+
+
+          <footer>
+            A simple project by <a href="https://github.com/lalitpatil891" target="_blank">Lalit Patil </a>– Keep Counting! &#10084;
+          </footer>
+        </div>
+      </div>
+    </>
+  )
+}
